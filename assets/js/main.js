869475b150b58cdb9193b221446f7678f9c239e1
Lucas Lyra <lucas10lyra@gmail.com>
2024-11-18 22:44:19 +0000

    Alterações para bom funcionamento de API

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,12 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => {
+        console.log(jsonBody); // Exibe todo o JSON recebido
+        return jsonBody.results; // Retorna a lista de Pokémon para o próximo then
+    })
+    .then((pokemonList) => {
+        debugger
+        console.log(pokemonList); // Exibe a lista de Pokémon
+    })
     .catch((error) => console.log(error));
