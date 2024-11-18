458f279c603a432cc810256618802837f901a8b9
Lucas Lyra <lucas10lyra@gmail.com>
2024-11-18 20:42:32 +0000

    Inicio de uso de promisse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,8 @@
-const offset ='0'
-const limit ='10'
-const url = `https://pokeapi.co/api/v2/pokemon?${offset}&${limit}`
+const offset = '0';
+const limit = '10';
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-
-fetch(url).then( function(response){
-    console.log(response)
-})
-.catch(function (error){
-    console.error(error)
-} )
-.finally(function (){
-    console.log("Requisição completa")
-})
-
-
-
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error));
